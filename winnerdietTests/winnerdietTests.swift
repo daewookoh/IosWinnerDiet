91e6d3dc1d21da129c490fd036429c97e7b0f659
godowondev <bdkfasd@naver.com>
2018-11-08 10:54:06 +0900

    Initial Commit

---
+++ winnerdietTests/winnerdietTests.swift
@@ -0,0 +1,34 @@
+//
+//  winnerdietTests.swift
+//  winnerdietTests
+//
+//  Created by godowondev on 2018. 11. 8..
+//  Copyright © 2018년 Dreamteams. All rights reserved.
+//
+
+import XCTest
+@testable import winnerdiet
+
+class winnerdietTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
